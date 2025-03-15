268c47c9d08e0c37e2e45b8f06a9501550d3b6f8
saj33l <sajeel.khan01@gmail.com>
2025-03-16 00:08:27 +0500

    Initial Programming

---
+++ main.py
@@ -0,0 +1,44 @@
+"""
+This is a simple program that demonstrates the use of the print function in Pythonw
+"""
+print(3)
+print(1+1)
+print(3*4)
+print(10/2)
+print(52*3 + 12*9)
+print((52*3)+(12*9))
+print(52*(3+12)*9)
+
+"""
+Number of minuutes in a week
+"""
+print(7*24*60)
+
+"""
+Using variables
+"""
+speed_of_light = 299792458
+print(speed_of_light*100)
+
+# distance light will travel in 1 year
+print(speed_of_light*365*24*60*60)
+
+# use of operators on variables
+days = 7*7
+days = 48
+days = days-1
+days = days+1
+
+minutes = 59
+minutes = minutes + 1
+seconds = minutes * 60
+print(seconds)
+
+""""
+Quiz
+Write Python code that defines the variable age to be your age in years, and then prints out the
+number of days you have been alive. If you don't want to use your real age, feel free to use your
+age in spirit instead.
+"""
+
+
